2a81c6da645b430a6b2311de518a5a8d7868615f
Luiz Lacerda <luizfelipe_lacerda@ymail.com>
2020-10-20 16:19:38 -0300

    erro besta2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/calendar'));
 app.get('/*', (req, res)=>{
-    res.sendFile(__dirname + '/dist/calendar/index.html');
+    res.sendFile(__dirname + '/dist/calendar/src/index.html');
 });
 
 app.listen(PORT, ()=>{
